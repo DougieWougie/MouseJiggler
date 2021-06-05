9ae44e60b026e231cef80a917db037688a5676a5
Dougie Richardson <dougthegreenie@gmail.com>
2021-06-05 14:01:44 +0100

    Update code.py
    Better utilise the Neopixels.

--- m0/code.py
+++ m0/code.py
@@ -1,26 +1,45 @@
-'''
-CircuitPython code to jiggle the mouse 
-'''
+"""CircuitPython code to jiggle the mouse"""
 
 import board
 import neopixel
 import usb_hid
 from time import sleep
 from adafruit_hid.mouse import Mouse
+from random import randint
+from _pixelbuf import colorwheel
 
-HOW_LONG = 180                # How long between jiggles in seconds
+HOW_LONG = 180     # How long between jiggles in seconds
 
 mouse = Mouse(usb_hid.devices)
 
-pixels = neopixel.NeoPixel(board.NEOPIXEL, 1)
+pixels = neopixel.NeoPixel(board.NEOPIXEL, 4, auto_write=False)
 pixels.brightness = 0.02
 
+def multi():
+    color = colorwheel(randint(1, 255))
+    for cycle in range(4):
+        for led in range(4):
+            pixels[led] = 0
+        pixels[cycle] = color
+        pixels.show()
+        sleep(0.1)
+
+def green():
+    pixels.fill((0, 255, 0))
+    pixels.show()
+    sleep(0.1)
+
+def jiggle():
+    for each in range(randint(1, 4)):
+        x = randint(1, 50)
+        y = randint(1, 50)
+        mouse.move(x, y)
+        mouse.move(-x, -y)
+
+green()
+
 while True:
-    pixels.fill((0, 255, 0))  # Green LED show's working
-    sleep(HOW_LONG)                # Wait HOW_LONG
-    pixels.fill((255, 0, 0))  # Red LED before when jiggling
-    mouse.move(10, 10)
-    mouse.move(-5, 5)
-    mouse.move(-10, -10)
-    mouse.move(5, 5)
-    sleep(1)                  # So the light is seen
+    jiggle()
+    multi()
+    green()
+    sleep(HOW_LONG)    # Wait HOW_LONG
