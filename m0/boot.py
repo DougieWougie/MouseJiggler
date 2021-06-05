376d3bbb014b77146e3ec0c4cbc600bd9a973d4a
Dougie Richardson <dougthegreenie@gmail.com>
2021-06-05 14:09:22 +0100

    Update boot.py
    Added method to prevent disabling REPL/storage.

--- m0/boot.py
+++ m0/boot.py
@@ -1,13 +1,24 @@
 '''
 Here be sharks...
 
-Remember this disables identifying as USB or REPL so it works on locked down devices (assuming the allow HID).
+Remember this disables identifying as USB or REPL so it works on locked down devices (assuming the allow HID). Touching either button on boot will stop it!
 
 More here https://learn.adafruit.com/customizing-usb-devices-in-circuitpython/circuitpy-midi-serial#dont-lock-yourself-out-3096636-14
 '''
 
 import storage
 import usb_cdc
+import board
+import touchio
 
-storage.disable_usb_drive()    # Disable CIRCUITPY
-usb_cdc.disable() 
+left = touchio.TouchIn(board.TOUCH1)
+right = touchio.TouchIn(board.TOUCH2)
+
+def disable():
+    storage.disable_usb_drive()    # Disable USB storage
+    usb_cdc.disable()              # Disable REPL
+
+if not left.value:
+    disable()
+elif not right.value:
+    disable()
