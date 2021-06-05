a4f6639e214b9b8e6b5be3755cf0956493529b74
Dougie Richardson <dougthegreenie@gmail.com>
2021-06-05 14:51:35 +0100

    Update boot.py
    Method to bypass boot.py as default.

--- m0/boot.py
+++ m0/boot.py
@@ -1,24 +1,30 @@
-'''
+"""
 Here be sharks...
 
-Remember this disables identifying as USB or REPL so it works on locked down devices (assuming the allow HID). Touching either button on boot will stop it!
+Remember this disables identifying as USB or REPL so it works on locked down devices (assuming the allow HID).
+Insert and then press both touchpads to enable as HID only device. 
 
 More here https://learn.adafruit.com/customizing-usb-devices-in-circuitpython/circuitpy-midi-serial#dont-lock-yourself-out-3096636-14
-'''
+"""
 
-import storage
-import usb_cdc
 import board
 import touchio
+from time import sleep
+import storage
+import usb_cdc
 
-left = touchio.TouchIn(board.TOUCH1)
-right = touchio.TouchIn(board.TOUCH2)
+touch1 = touchio.TouchIn(board.TOUCH1)
+touch2 = touchio.TouchIn(board.TOUCH2)
 
 def disable():
     storage.disable_usb_drive()    # Disable USB storage
     usb_cdc.disable()              # Disable REPL
 
-if not left.value:
+def enable():
+    print("enable")
+
+sleep(3)    # Without a pause, the system never detects both pads
+if touch1.value and touch2.value:
     disable()
-elif not right.value:
-    disable()
+else:
+    enable()
